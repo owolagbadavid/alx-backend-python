4b6e3476dbb8c0350a98b4c62d217551effb1831
DAVID <owolagbadavid@gmail.com>
2024-04-22 21:28:49 +0100

    fix

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int):
     """
     asynchronous coroutine that takes in 2 arguments
     and returns a float
